a08b03f38d60d91a1b91595499f087c51c0db3b9
sreeharisreehari <kisreehari52@gmail.com>
2024-08-27 15:11:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import Example from './components/Header'
+
+function App() {
+ 
+
+  return (
+    <div >
+  <Example/>
+     
+    </div>
+  )
+}
+
+export default App
